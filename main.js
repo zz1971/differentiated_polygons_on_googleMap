24af95839b727497bf01dff6c1e93f129c7d6994
zz1971 <Zohreh@users.noreply.github.com>
2021-03-11 04:39:19 -0500

    adding my new project codes into repo.

---
+++ main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+function initMap(cnt) {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: cnt,
+        mapTypeId: "terrain",
+    });
+    return map;
+}
+
+function drawPolygon(path, map, k) {
+    var satOrLight = (k % 100);
+    var h = 360 - k;
+    var pol = new google.maps.Polygon({
+        paths: path,
+        strokeColor: 'hsl(' + h + ',70%,40%)',
+        strokeOpacity: 1,
+        strokeWeight: 1.4,
+        fillColor: 'hsl(' + k + ',80%,' + satOrLight + '%)',
+        fillOpacity: 1,
+    });
+    pol.setMap(map);
+    console.log(k, "100%", satOrLight);
+    return pol;
+}
+
+////////////////////////Next Step: Attach Info Window////////////////////////////
+function attachPolygonInfoWindow(array, html, map) {
+    array.infoWindow = new google.maps.InfoWindow({
+        content: html,
+    });
+    google.maps.event.addListener(array, 'mouseover', function (e) {
+        var latLng = e.latLng;
+        array.infoWindow.setPosition(latLng);
+        array.infoWindow.open(map);
+    });
+    google.maps.event.addListener(array, 'mouseout', function () {
+        array.infoWindow.close();
+    });
+}
+
+function initialMap() {
+
+    var map = initMap({
+        lng: -75.16660261191,
+        lat: 39.9618914644567,
+    });
+
+    var arr = [];
+    while (arr.length < geojson.features.length) {
+        var r = Math.floor(Math.random() * 360) + 1;
+        if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    //console.log(arr);
+
+    for (var j = 0; j < geojson.features.length; j++) {
+        var array = [];
+        for (var i = 0; i < geojson.features[j].geometry.coordinates[0].length; i++) {
+            array[i] = {
+                lng: geojson.features[j].geometry.coordinates[0][i][0],
+                lat: geojson.features[j].geometry.coordinates[0][i][1],
+            }
+        }
+        map.setCenter(new google.maps.LatLng(array[0].lat, array[0].lng));
+        var hueNumber = arr[j];
+        var polStore = drawPolygon(array, map, hueNumber);
+        var infoHtml = "<div class='info'><div>Address :" + geojson.features[j].properties.ADDRESS + "</div><div>Building Name :" + geojson.features[j].properties.BUILDING_NAME + "</div><div>" + geojson.features[j].properties.BASE_ELEVATION + "</div><div>" + geojson.features[j].properties.APPROX_HGT + "</div><div>" + geojson.features[j].properties.MAX_HGT + "</div><div>" + geojson.features[j].properties.PARCEL_ID_NUM + "</div><div>" + geojson.features[j].properties.PARCEL_ID_SOURCE + "</div><div>" + geojson.features[j].properties.Shape__Area + "</div></div>"
+        attachPolygonInfoWindow(polStore, infoHtml, map);
+    }
+}
